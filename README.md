24ba44653f931d79a8ed8689af64a9530b03d57b
Matteo Migliorini <matteo.migliorini95@gmail.com>
2024-03-14 23:34:48 +0100

    First example of simple analyzer

---
+++ README.md
@@ -0,0 +1,49 @@
+# Example L1Trigger Scouting Analyzer
+
+This repo contains a simple example of an EDAnalyzer used to analyze L1T Scouting data collected during 2023.
+It will produce a ROOT file containing 3 histograms:
+1. The BX occupancy in the orbit
+2. Pt distribution of the muons (example of how to manipulate objects)
+3. The BX occupancy such that at least a muon is present and there are no jets in the previous BX (example of how to use objects from a different BX)
+
+## 2023 L1 Scouting data
+Data available in DAS, more info in [this](https://mattermost.web.cern.ch/cms-l1-scouting/channels/town-square) slide.
+```
+file dataset=/L1ScoutUGMTCALO/Run2023C-v1/RAW run=368636
+```
+
+Event content:
+```
+$ edmDumpEventContent root://cms-xrd-global.cern.ch//store/data/Run2023C/L1ScoutUGMTCALO/RAW/v1/000/368/636/00000/run368636_ls0400.root
+Type                                  Module             Label     Process   
+-----------------------------------------------------------------------------
+OrbitCollection<l1ScoutingRun3::BxSums>    "CaloUnpacker"     ""        "SCPU"    
+OrbitCollection<l1ScoutingRun3::EGamma>    "CaloUnpacker"     ""        "SCPU"    
+OrbitCollection<l1ScoutingRun3::Jet>    "CaloUnpacker"     ""        "SCPU"    
+OrbitCollection<l1ScoutingRun3::Muon>    "GmtUnpacker"      ""        "SCPU"    
+OrbitCollection<l1ScoutingRun3::Tau>    "CaloUnpacker"     ""        "SCPU"
+```
+
+Each event contains data from one orbit, i.e. a collection of BX, and are stored in an [OrbitCollection](https://github.com/cms-sw/cmssw/blob/master/DataFormats/L1Scouting/interface/OrbitCollection.h).
+
+## Instructions for the demo
+
+```
+# create a project area
+cmsrel CMSSW_14_1_0_pre1 
+
+cd CMSSW_14_1_0_pre1/src
+cmsenv
+
+mkdir Demo
+cd Demo
+
+git clone https://github.com/Mmiglio/L1ScoutingAnalyzer DemoAnalyzer
+
+cd DemoAnalyzer
+scram b
+
+# process 10k orbits
+cmsRun python/demo_cfg.py inFile=root://cms-xrd-global.cern.ch//store/data/Run2023C/L1ScoutUGMTCALO/RAW/v1/000/368/636/00000/run368636_ls0400.root outFile=test.root numOrbits=10000
+```
+
---
+++ plugins/DemoAnalyzer.cc
@@ -0,0 +1,242 @@
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/global/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/Utilities/interface/EDGetToken.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Utilities/interface/StreamID.h"
+#include "FWCore/Utilities/interface/Span.h"
+
+// root include files
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "TH1.h"
+#include "TH1D.h"
+#include "TH2F.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TBranch.h"
+#include "TDirectory.h"
+
+// L1 scouting 
+#include "DataFormats/L1Scouting/interface/L1ScoutingMuon.h"
+#include "DataFormats/L1Scouting/interface/L1ScoutingCalo.h"
+#include "DataFormats/L1Scouting/interface/OrbitCollection.h"
+#include "L1TriggerScouting/Utilities/interface/conversion.h"
+#include "L1TriggerScouting/Utilities/interface/convertToL1TFormat.h"
+
+// l1trigger
+#include "DataFormats/L1Trigger/interface/Muon.h"
+#include "DataFormats/L1Trigger/interface/Jet.h"
+#include "DataFormats/L1Trigger/interface/EGamma.h"
+#include "DataFormats/L1Trigger/interface/Tau.h"
+#include "DataFormats/L1Trigger/interface/EtSum.h"
+
+
+#include <memory>
+#include <utility>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace l1ScoutingRun3;
+
+class DemoAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources> {
+public:
+  explicit DemoAnalyzer(const edm::ParameterSet&);
+  ~DemoAnalyzer() {}
+  static void fillDescriptions(edm::ConfigurationDescriptions&);
+
+private:
+  void analyze(const edm::Event&, const edm::EventSetup&) override;
+  void beginJob() override;
+  void endJob() override;
+
+  void processDataBx(
+    unsigned bx,
+    const edm::Handle<MuonOrbitCollection>& muonsCollection,
+    const edm::Handle<JetOrbitCollection>& jetsCollection,
+    const edm::Handle<EGammaOrbitCollection>& eGammasCollection,
+    const edm::Handle<TauOrbitCollection>& tausCollection,
+    const edm::Handle<BxSumsOrbitCollection>& bxSumsCollection
+  );
+
+  // tokens for scouting data
+  edm::EDGetTokenT<OrbitCollection<l1ScoutingRun3::Muon>> muonsTokenData_;
+  edm::EDGetTokenT<OrbitCollection<l1ScoutingRun3::Jet>> jetsTokenData_;
+  edm::EDGetTokenT<OrbitCollection<l1ScoutingRun3::EGamma>> eGammasTokenData_;
+  edm::EDGetTokenT<OrbitCollection<l1ScoutingRun3::Tau>> tausTokenData_;
+  edm::EDGetTokenT<OrbitCollection<l1ScoutingRun3::BxSums>> bxSumsTokenData_;
+
+  // Map for input tags
+  std::map<std::string, edm::InputTag> m_inputTags;
+
+  // the root file service to handle the output file
+  edm::Service<TFileService> fs;
+
+  // l1t standard data format
+  std::vector<l1t::Jet> l1jets_;
+  std::vector<l1t::EGamma> l1egs_;
+  std::vector<l1t::EtSum> l1sums_;
+  std::vector<l1t::Tau> l1taus_;
+  std::vector<l1t::Muon> l1muons_;
+
+  // map containing TH1D histograms
+  std::map<std::string, TH1D*> m_1dhist_;
+ };
+
+DemoAnalyzer::DemoAnalyzer(const edm::ParameterSet& iPSet){
+  
+  // get the input tags
+  m_inputTags["muonsTag_"] = iPSet.getParameter<edm::InputTag>("muonsTag");
+  m_inputTags["jetsTag_"] = iPSet.getParameter<edm::InputTag>("jetsTag");
+  m_inputTags["eGammasTag_"] = iPSet.getParameter<edm::InputTag>("eGammasTag");
+  m_inputTags["tausTag_"] = iPSet.getParameter<edm::InputTag>("tausTag");
+  m_inputTags["bxSumsTag_"] = iPSet.getParameter<edm::InputTag>("bxSumsTag");
+
+  // based on the data type, get tokens
+  muonsTokenData_ = consumes<OrbitCollection<l1ScoutingRun3::Muon>>(m_inputTags["muonsTag_"]);
+  jetsTokenData_ = consumes<OrbitCollection<l1ScoutingRun3::Jet>>(m_inputTags["jetsTag_"]);
+  eGammasTokenData_ = consumes<OrbitCollection<l1ScoutingRun3::EGamma>>(m_inputTags["eGammasTag_"]);
+  tausTokenData_ = consumes<OrbitCollection<l1ScoutingRun3::Tau>>(m_inputTags["tausTag_"]);
+  bxSumsTokenData_ = consumes<OrbitCollection<l1ScoutingRun3::BxSums>>(m_inputTags["bxSumsTag_"]);
+
+  // init internal containers for l1 objects
+  l1muons_.reserve(8);
+  l1jets_.reserve(12);
+  l1egs_.reserve(12);
+  l1taus_.reserve(12);
+  l1sums_.reserve(12);
+  
+  // create histogram subdir
+  TFileDirectory histoSubDir = fs->mkdir("histograms");
+
+  // Init histograms
+  m_1dhist_["MuonBxOcc"] = histoSubDir.make<TH1D>("MuonBxOcc", "BX in orbit with at least one muon", 3566, -0.5, 3565.5);
+  m_1dhist_["Muon_JetVeto_BxOcc"] = histoSubDir.make<TH1D>("Muon_JetVeto_BxOcc", "BX in orbit with at least one muon and no jet in BX-1", 3566, -0.5, 3565.5);
+  m_1dhist_["MuonPt"] = histoSubDir.make<TH1D>("MuonPt", "MuonPt", 512, 0, 256);
+
+}
+
+void DemoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
+  edm::Handle<OrbitCollection<l1ScoutingRun3::Muon>> muonsCollection; 
+  edm::Handle<OrbitCollection<l1ScoutingRun3::Jet>> jetsCollection; 
+  edm::Handle<OrbitCollection<l1ScoutingRun3::EGamma>> eGammasCollection; 
+  edm::Handle<OrbitCollection<l1ScoutingRun3::Tau>> tausCollection; 
+  edm::Handle<OrbitCollection<l1ScoutingRun3::BxSums>> bxSumsCollection; 
+
+  iEvent.getByToken(muonsTokenData_, muonsCollection); 
+  iEvent.getByToken(jetsTokenData_, jetsCollection); 
+  iEvent.getByToken(eGammasTokenData_, eGammasCollection); 
+  iEvent.getByToken(tausTokenData_, tausCollection); 
+  iEvent.getByToken(bxSumsTokenData_, bxSumsCollection); 
+
+  // get orbit number orbit
+  // unsigned orbitNum = iEvent.id().event();
+
+  // process all BX in orbit containing at least a Muon
+  // getFilledBxs() returns the list of filled BX in the muon orbit collection
+  for (const unsigned& bx : muonsCollection->getFilledBxs()) {
+    processDataBx(
+        bx,
+        muonsCollection,
+        jetsCollection,
+        eGammasCollection,
+        tausCollection,
+        bxSumsCollection
+    ); 
+  }
+  
+ }
+
+void DemoAnalyzer::processDataBx(
+    unsigned bx,
+    const edm::Handle<MuonOrbitCollection>& muonsCollection,
+    const edm::Handle<JetOrbitCollection>& jetsCollection,
+    const edm::Handle<EGammaOrbitCollection>& eGammasCollection,
+    const edm::Handle<TauOrbitCollection>& tausCollection,
+    const edm::Handle<BxSumsOrbitCollection>& bxSumsCollection
+  ) {
+    
+    // get iterator for the current BX
+    const auto& jets = jetsCollection->bxIterator(bx);
+    const auto& eGammas = eGammasCollection->bxIterator(bx);
+    const auto& taus = tausCollection->bxIterator(bx);
+    const auto& bxSums = bxSumsCollection->bxIterator(bx);
+    const auto& muons = muonsCollection->bxIterator(bx);
+
+    // convert scouting objects to l1t::objects for semplicity
+    // Note: Scouting objects are stored in hw quantities, if only a subset
+    // of the features is needed, the function in L1TriggerScouting/Utilities/interface/conversion.h
+    // can be used to get physical quantities.
+    // for example, the momentum of a muon can be obtained with ugmt::fPt(hwPt);
+    l1jets_.clear();
+    l1egs_.clear();
+    l1taus_.clear();
+    l1sums_.clear();
+    l1muons_.clear();
+
+    for (const auto& muon : muons) {
+      l1muons_.emplace_back(getL1TMuon(muon));
+    }
+    for (const auto& jet : jets) {
+      l1jets_.emplace_back(getL1TJet(jet));
+    }
+    for (const auto& egamma : eGammas) {
+      l1egs_.emplace_back(getL1TEGamma(egamma));
+    }
+    for (const auto& tau : taus) {
+      l1taus_.emplace_back(getL1TTau(tau));
+    }    
+
+    // store some of the sums
+    if (bxSums.size()>0){
+      l1sums_.emplace_back(getL1TEtSum(bxSums[0], l1t::EtSum::EtSumType::kTotalEt));
+      l1sums_.emplace_back(getL1TEtSum(bxSums[0], l1t::EtSum::EtSumType::kTotalHt));
+      l1sums_.emplace_back(getL1TEtSum(bxSums[0], l1t::EtSum::EtSumType::kMissingEt));
+      l1sums_.emplace_back(getL1TEtSum(bxSums[0], l1t::EtSum::EtSumType::kMissingHt));
+      l1sums_.emplace_back(getL1TEtSum(bxSums[0], l1t::EtSum::EtSumType::kTowerCount));
+    }
+
+    // fill histograms
+    m_1dhist_["MuonBxOcc"]->Fill(bx);
+    for (const auto& muon: l1muons_){
+      m_1dhist_["MuonPt"]->Fill(muon.pt());
+    }
+    
+    // require no jets in the previous BX
+    // check that we are not in the first BX. There could be halo muons from non colliding
+    // bunches / cosmics.
+    if (bx>1){
+      // we can access the jets (if any) with jetsCollection->bxIterator(bx-1);
+      // for this example, we will just check how many of them there are in bx-1
+      if (jetsCollection->getBxSize(bx-1)>0){
+        m_1dhist_["Muon_JetVeto_BxOcc"]->Fill(bx); 
+
+        // fill more hists, do something else....
+      }
+    }
+
+}
+
+void DemoAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+ 
+// ------------ method called once each job just before starting event loop  ------------
+void DemoAnalyzer::beginJob() {
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void DemoAnalyzer::endJob() {
+}
+
+DEFINE_FWK_MODULE(DemoAnalyzer);
